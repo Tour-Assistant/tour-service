539deabd4a85ee498bb868ab4d57b269724cd3ac
bmshamsnahid <b.m.shamsnahid@gmail.com>
2021-07-28 00:54:10 +0600

    Added types

--- src/types/tour.ts
+++ src/types/tour.ts
@@ -1,11 +1,38 @@
 export type EVENT_STATUS = "UPCOMING" | "CLOSED";
 
+interface PLACE {
+  name: string;
+  alternativeNames?: [string];
+  postCode: number;
+  area?: string;
+  thana?: string;
+  district?: string;
+  division?: string;
+  country?: string;
+}
+
+export interface HOST_AUTHORITY {
+  name: string;
+  phone: string;
+}
+
+export interface HOSTED_BY {
+  name: string;
+  link: {
+    page: string;
+    group: string;
+  };
+  authority: [HOST_AUTHORITY];
+}
+
 export interface Tour {
   id: string;
   title: string;
   eventStatus: EVENT_STATUS;
   startAt: string;
   createdAt: string;
+  hostedBy?: HOST_AUTHORITY;
+  places: [PLACE];
 }
 
 export interface CreateTourPostRequest {
